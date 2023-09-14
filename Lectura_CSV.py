30d7dc9214bb48941a4f7b0d3de87a0f77a66d91
SofiaQuijano <142341037+SofiaQuijano@users.noreply.github.com>
2023-09-14 13:12:24 -0500

    Update Lectura_CSV.py

--- Lectura_CSV.py
+++ Lectura_CSV.py
@@ -1,1 +1,23 @@
+#Importación
+import cvs
 
+#Nombre del archivo cvs
+filename = "LecturaE.csv"
+
+#Inicializar titulos y lista de filas
+Fields = []
+rows = []
+
+#Lectura del archivo csv
+with open(filename, "r") as cvsfile
+
+#crar ojeto de lectura de cvs
+cvsreader = cvs.reader(cvsfile)
+
+#Extracción de nombres de campos a través de la primera fila
+fields = next(cvsreader)
+
+#Extraer cada fila de datos una a una
+for row in csvreader:
+    rows.append(row)
+  
